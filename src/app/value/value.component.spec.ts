639a079d9638e30c035ac8722cc746ee89225a6e
Oleh Povshednyi <powshednyioleg@gmail.com>
2020-04-02 16:57:04 +0300

    walking skeleton

--- src/app/value/value.component.spec.ts
+++ src/app/value/value.component.spec.ts
@@ -1,19 +1,18 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
 
-import { ValueComponent } from './value.component';
+import { ValueComponent } from "./value.component";
 
-describe('ValueComponent', () => {
+describe("ValueComponent", () => {
   let component: ValueComponent;
   let fixture: ComponentFixture<ValueComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ValueComponent ]
-    })
-    .compileComponents();
+      declarations: [ValueComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,7 +21,7 @@ describe('ValueComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/value/value.component.ts
+++ src/app/value/value.component.ts
@@ -2,27 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-	selector: 'app-value',
-	templateUrl: './value.component.html',
-	styleUrls: ['./value.component.css']
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
 })
 export class ValueComponent implements OnInit {
-	values: any;
+  values: any;
 
-	constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-	ngOnInit() {
-		this.getValues();
-	}
+  ngOnInit() {
+    this.getValues();
+  }
 
-	getValues() {
-		this.http.get('https://localhost:44367/api/values').subscribe(
-			response => {
-				this.values = response;
-			},
-			error => {
-				console.log(error);
-			}
-		);
-	}
+  getValues() {
+    this.http.get('https://localhost:44367/api/values').subscribe(
+      response => {
+        this.values = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
